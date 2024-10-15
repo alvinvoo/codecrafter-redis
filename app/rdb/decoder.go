56cb8023be8e65663c37654143e8005ea7364a0e
alvin.voo <alvin.voo@gmail.com>
2024-10-15 18:04:06 +0800

    finished decoder

--- app/rdb/decoder.go
+++ app/rdb/decoder.go
@@ -3,19 +3,45 @@ package rdb
 import (
 	"bufio"
 	"bytes"
+	"encoding/binary"
 	"fmt"
 	"io"
+	"log"
 	"strconv"
 
 	"github.com/codecrafters-io/redis-starter-go/app/util"
 )
 
+const (
+	rdb6bitLen  = 0
+	rdb14bitLen = 1
+	rdb32bitLen = 2
+	rdbEncVal   = 3
+
+	rdbFlagMeta     = 0xfa
+	rdbFlagResizeDB = 0xfb
+	rdbFlagExpiryMS = 0xfc
+	rdbFlagExpiryS  = 0xfd
+	rdbFlagSelectDB = 0xfe
+	rdbFlagEOF      = 0xff
+
+	rdbEncInt8  = 0
+	rdbEncInt16 = 1
+	rdbEncInt32 = 2
+)
+
 type Decoder interface {
-	StartDatabase(n int)
+	StartRDB()
+
+	StartDatabase(n uint32)
 
 	Meta(key, value []byte)
 
 	Set(key, value []byte, expiry int64)
+
+	EndDatabase(n uint32)
+
+	EndRDB()
 }
 
 type byteReader interface {
@@ -34,8 +60,85 @@ func (d *decode) decode() error {
 	if err != nil {
 		return err
 	}
+	d.event.StartRDB()
 
-	return nil
+	var objType byte
+	var key, value []byte
+	var expiry int64
+	var db uint32
+
+	for {
+
+		objType, err = d.r.ReadByte()
+		if err != nil {
+			return err
+		}
+
+		switch objType {
+		case rdbFlagMeta:
+			key, err = d.readString()
+			if err != nil {
+				return err
+			}
+
+			value, err = d.readString()
+			if err != nil {
+				return err
+			}
+
+			d.event.Meta(key, value)
+
+		case rdbFlagResizeDB:
+			htSize, _, err := d.readLength()
+			if err != nil {
+				return err
+			}
+
+			htExpirySize, _, err := d.readLength()
+			if err != nil {
+				return err
+			}
+
+			log.Printf("resizeDB hash table size %d expiry size %d\n", htSize, htExpirySize)
+		case rdbFlagExpiryMS:
+			// intBuf is 8 bytes as initiliazed in Decoder
+			// but just to make it clear
+			_, err := io.ReadFull(d.r, d.intBuf[:8])
+			if err != nil {
+				return err
+			}
+			expiry = int64(binary.LittleEndian.Uint64(d.intBuf))
+		case rdbFlagExpiryS:
+			_, err := io.ReadFull(d.r, d.intBuf[:4])
+			if err != nil {
+				return err
+			}
+			expiry = int64(binary.LittleEndian.Uint32(d.intBuf)) * 1000
+		case rdbFlagSelectDB:
+			db, _, err = d.readLength()
+			if err != nil {
+				return err
+			}
+
+			d.event.StartDatabase(db)
+		case rdbFlagEOF:
+			d.event.EndDatabase(db)
+			d.event.EndRDB()
+			return nil
+		default: // default here is "00"
+			key, err = d.readString()
+			if err != nil {
+				return err
+			}
+
+			value, err = d.readString()
+			if err != nil {
+				return err
+			}
+
+			d.event.Set(key, value, expiry)
+		}
+	}
 }
 
 func (d *decode) checkHeader() error {
@@ -59,8 +162,86 @@ func (d *decode) checkHeader() error {
 	return nil
 }
 
-func (d *decode) readLength() {
-	// TODO
+// max is 32 bit
+func (d *decode) readLength() (uint32, bool, error) {
+	b, err := d.r.ReadByte()
+	if err != nil {
+		return 0, false, err
+	}
+
+	switch (b & 0xC0) >> 6 { // the bitwise AND part is redundant, but i guess its to improve readability
+	case rdb6bitLen:
+		return uint32(b & 0x3f), false, nil
+	case rdb14bitLen:
+		bb, err := d.r.ReadByte()
+		if err != nil {
+			return 0, false, err
+		}
+
+		// big endian
+		return (uint32(b&0x3f)<<8 | uint32(bb)), false, nil
+	case rdb32bitLen:
+		_, err := io.ReadFull(d.r, d.intBuf[:4])
+		if err != nil {
+			return 0, false, err
+		}
+
+		return binary.BigEndian.Uint32(d.intBuf[:4]), false, nil
+	case rdbEncVal:
+		return uint32(b & 0x3f), true, nil
+	default:
+		return 0, false, fmt.Errorf("length encoding not supported")
+	}
+}
+
+func (d *decode) readString() ([]byte, error) {
+	length, encode, err := d.readLength()
+	if err != nil {
+		return nil, err
+	}
+
+	if encode {
+		switch length {
+		case rdbEncInt8:
+			i, err := d.readUint8()
+			return []byte(strconv.Itoa(int(i))), err
+		case rdbEncInt16:
+			i, err := d.readUint16()
+			return []byte(strconv.Itoa(int(i))), err
+		case rdbEncInt32:
+			i, err := d.readUint32()
+			return []byte(strconv.Itoa(int(i))), err
+		default:
+			return nil, fmt.Errorf("string encoding not supported")
+		}
+	}
+
+	str := make([]byte, length)
+	_, err = io.ReadFull(d.r, str)
+	return str, err
+}
+
+func (d *decode) readUint8() (uint8, error) {
+	b, err := d.r.ReadByte()
+	return uint8(b), err
+}
+
+func (d *decode) readUint16() (uint16, error) {
+	_, err := io.ReadFull(d.r, d.intBuf[:2])
+	if err != nil {
+		return 0, err
+	}
+
+	return binary.LittleEndian.Uint16(d.intBuf[:2]), nil
+}
+
+func (d *decode) readUint32() (uint32, error) {
+	_, err := io.ReadFull(d.r, d.intBuf[:4])
+	if err != nil {
+		return 0, err
+	}
+
+	return binary.LittleEndian.Uint32(d.intBuf[:4]), nil
 }
 
 // Decode parses a RDB file from r and calls the decode hooks on d.
--- app/rdb/storage.go
+++ app/rdb/storage.go
@@ -8,12 +8,11 @@ import (
 )
 
 type StoreWorker struct {
-	db int
+	db uint32
 }
 
 type option struct {
-	storedAt  time.Time
-	expiresIn time.Duration
+	expiresAt int64
 }
 
 type store struct {
@@ -23,7 +22,11 @@ type store struct {
 
 var items = make(map[string]store)
 
-func (p *StoreWorker) StartDatabase(n int) {
+func (p *StoreWorker) StartRDB() {
+	util.DebugLog("StartRDB: Start parsing RDB")
+}
+
+func (p *StoreWorker) StartDatabase(n uint32) {
 	p.db = n
 }
 
@@ -33,26 +36,50 @@ func (p *StoreWorker) Meta(k, v []byte) {
 
 func (p *StoreWorker) Set(k, v []byte, expiry int64) {
 	util.DebugLog("Set ", fmt.Sprintf("k %v v %v expiry %v", k, v, expiry))
+	storeRDB(string(k), v, expiry)
 }
 
-// expiry is in miliseconds
-func StoreRDB(k string, v []byte, expiry int64) error {
+func (p *StoreWorker) EndDatabase(n uint32) {
+	p.db = n
+}
+
+func (p *StoreWorker) EndRDB() {
+	util.DebugLog("EndRDB: End parsing RDB")
+}
+
+func storeRDB(k string, v []byte, expiry int64) {
 	if expiry == 0 {
 		items[k] = store{
 			data: v,
 		}
 	} else {
-		duration, err := time.ParseDuration(fmt.Sprintf("%dms", expiry))
+		items[k] = store{
+			data: v,
+			option: &option{
+				expiresAt: expiry,
+			},
+		}
+	}
+}
+
+// expiryDuration is in miliseconds
+func StoreRDB(k string, v []byte, expiryDuration int64) error {
+	if expiryDuration == 0 {
+		items[k] = store{
+			data: v,
+		}
+	} else {
+		duration, err := time.ParseDuration(fmt.Sprintf("%dms", expiryDuration))
 
 		if err != nil {
 			return err
 		}
 
+		expiresAt := time.Now().Add(duration).UnixMilli()
 		items[k] = store{
 			data: v,
 			option: &option{
-				storedAt:  time.Now(),
-				expiresIn: duration,
+				expiresAt: expiresAt,
 			},
 		}
 	}
@@ -60,11 +87,11 @@ func StoreRDB(k string, v []byte, expiry int64) error {
 	return nil
 }
 
-func GetRDB(k string) (data []byte, ok bool) {
+func GetRDB(k string) ([]byte, bool) {
 	val, ok := items[k]
 
 	if val.option != nil {
-		if (val.option.storedAt.Add(val.option.expiresIn)).Before(time.Now()) {
+		if time.Now().UnixMilli() > val.option.expiresAt {
 			delete(items, k)
 			return nil, false
 		}
